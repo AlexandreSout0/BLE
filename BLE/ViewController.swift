cc972070bf494793a68434c72db053e7283e8f1a
HIDROXIDO <soutoinf@gmail.com>
2022-10-06 21:51:33 -0300

    ok

--- BLE/ViewController.swift
+++ BLE/ViewController.swift
@@ -6,9 +6,24 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
+    let client = BLEClient()
+    
+    @IBOutlet weak var textBox: UITextView!
+    
+    @IBAction func Receiver(_ sender: Any) {
 
+    }
+    
+    @IBAction func BLESent(_ sender: Any) {
+        client.sendText(text: "Hey Hello")
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +32,22 @@ class ViewController: UIViewController {
 
 }
 
+
+
+/*
+ // Create new Alert
+
+ var dialogMessage = UIAlertController(title: "Confirm", message: "Are you sure you want to delete this?", preferredStyle: .alert)
+ 
+ // Create OK button with action handler
+ let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+     print("Ok button tapped")
+  })
+ 
+ //Add OK button to a dialog message
+ dialogMessage.addAction(ok)
+
+ // Present Alert to
+ self.present(dialogMessage, animated: true, completion: nil)
+ 
+ */
